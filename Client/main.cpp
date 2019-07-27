db20a2a9718f7fcdc26055c8d274d8ab14ea67e0
Moaif <32565030+Moaif@users.noreply.github.com>
2019-07-27 13:38:30 +0200

    Second iteration on networking
    Now the UPD client waits till the server makes him a confirmation to know that is conected, and first approach to work with IP addesses(not tested yet)

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <WS2tcpip.h>
 #include <string>
+#include <ctime>
 
 #pragma comment (lib, "ws2_32.lib")
 
 using namespace std;
 
-static const int SERVER_BUFFER_SIZE = 1024;
-static const int CLIENT_INFO_BUFFER_SIZE = 256;
+#define DEFAULT_PORT "27015"
+
+static const float CONECTION_WAIT_TIME = 5.0f;
+static const int SERVER_MSG_BUFFER = 128;
 
 int main(int argc, char* argv[])
 {
+	// Validate the parameters
+	if(argc != 2)
+	{
+		printf("usage: %s server-name\n", argv[0]);
+		return 1;
+	}
+
 	//Startup Winsock
 	WSADATA data;
 	WORD version = MAKEWORD(2, 2);
@@ -18,26 +28,104 @@ int main(int argc, char* argv[])
 	if(wsOk != 0)
 	{
 		cout << "Can't start Winsock! " << wsOk << endl;
-		return  -1;
+		return  1;
 	}
 
-	//Create a hint structure for the server
-	sockaddr_in server;
-	server.sin_family = AF_INET;
-	server.sin_port = htons(54000);
-	inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
+	struct addrinfo hints;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
 
-	//Socket creation
-	SOCKET out = socket(AF_INET, SOCK_DGRAM, 0);
+	// Resolve the server address and port
+	struct addrinfo* result;
+	wsOk = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+	if(wsOk != 0)
+	{
+		printf("getaddrinfo failed with error: %d\n", wsOk);
+		WSACleanup();
+		return 1;
+	}
+
+	//Create socket & try to conect to UDP server
+	SOCKET out;
+	char buf[SERVER_MSG_BUFFER];
+	bool conected = false;
+	DWORD timeout = CONECTION_WAIT_TIME * 1000;
+	for(; result != NULL; result = result->ai_next)
+	{
+		out = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+		if(out == INVALID_SOCKET)
+		{
+			printf("socket failed with error: %ld\n", WSAGetLastError());
+			WSACleanup();
+			return 1;
+		}
+
+		//Send join request
+		ZeroMemory(buf, SERVER_MSG_BUFFER);
+		buf[0] = '1';
+		int sendOk = sendto(out, buf, SERVER_MSG_BUFFER, 0, result->ai_addr, static_cast<int>(result->ai_addrlen));
+
+		if(sendOk == SOCKET_ERROR)
+		{
+			cout << "That didn't work! " << WSAGetLastError() << endl;
+		}
+
+		//Wait for the server reply, otherwise its not conected
+		setsockopt(out, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof timeout);
+
+		wsOk = recv(out, buf, SERVER_MSG_BUFFER, 0);
+
+		if(wsOk == SOCKET_ERROR)
+		{
+			int error = WSAGetLastError();
+			if(error != WSAEWOULDBLOCK && error != WSAECONNRESET)
+			{
+				cout << "Waiting for conection returned SOCKET_ERROR" << WSAGetLastError() << endl;
+			}
+
+		}
+		//If the buffer is not empty
+		if(buf[0] != '\0')
+		{
+			if(buf[0] == 1)// 1 is for join , 2 for input(should be managed with enums
+			{				
+				cout << "Conected to server" << endl;
+				conected = true;
+				break;
+			}
+		}		
+	}
+
+	if(!conected)
+	{
+		cout << "No response from the server, not possible to connect" << endl;
+		WSACleanup();
+		return 1;
+	}
 
 	//Write out to that socket
-	string s(argv[1]);
-	int sendOk = sendto(out, s.c_str(), s.size() +1, 0, reinterpret_cast<sockaddr*>(&server), sizeof(server));
+	string s("");
+	do{
+		cout << "Enter text to server: ";
+		getline(cin, s);
+		if(s == "exit")
+		{
+			break;
+		}
+		s = '2' + s;//Add the type value for input
 
-	if(sendOk == SOCKET_ERROR)
-	{
-		cout << "That didn't work! " << WSAGetLastError() << endl;
-	}
+		int sendOk = sendto(out, s.c_str(), s.size() + 1, 0, result->ai_addr, static_cast<int>(result->ai_addrlen));
+
+		if(sendOk == SOCKET_ERROR)
+		{
+			cout << "That didn't work! " << WSAGetLastError() << endl;
+		}
+	}while(true);
+
+	
+	freeaddrinfo(result);
 
 	//Close the socket
 	closesocket(out);
--- Server/main.cpp
+++ Server/main.cpp
@@ -5,8 +5,11 @@
 
 using namespace std;
 
+#define DEFAULT_PORT "27015"
+
 static const int SERVER_BUFFER_SIZE = 1024;
 static const int CLIENT_INFO_BUFFER_SIZE = 256;
+static const int SERVER_ACCEPT_MSG_BUFFER = 128;
 
 int main(int argc, char* argv[])
 {
@@ -17,21 +20,43 @@ int main(int argc, char* argv[])
 	if(wsOk != 0)
 	{
 		cout << "Can't start Winsock! " << wsOk << endl;
-		return -1;
+		return 1;
 	}
 
-	//Bind socket to ip address and port
-	SOCKET in = socket(AF_INET, SOCK_DGRAM, 0);
-	sockaddr_in serverHint;
-	serverHint.sin_addr.S_un.S_addr = ADDR_ANY;
-	serverHint.sin_family = AF_INET;
-	serverHint.sin_port = htons(54000); //Conver from little to big endian
+	struct addrinfo hints;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+	hints.ai_flags = AI_PASSIVE;
 
-	if(bind(in, reinterpret_cast<sockaddr*>(&serverHint), sizeof(serverHint)) == SOCKET_ERROR)
+	// Resolve the server address and port
+	struct addrinfo *result;
+	wsOk = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if(wsOk != 0)
+	{
+		printf("getaddrinfo failed with error: %d\n", wsOk);
+		WSACleanup();
+		return 1;
+	}
+
+	// Create a SOCKET for connecting to server
+	SOCKET in = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if(in == INVALID_SOCKET)
+	{
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	if(bind(in, result->ai_addr, static_cast<int>(result->ai_addrlen)) == SOCKET_ERROR)
 	{
 		cout << "Can't bind socket! " << WSAGetLastError() << endl;
-		return -1;
+		return 1;
 	}
+
+	freeaddrinfo(result);
 
 	sockaddr_in client;
 	int clientLenght = sizeof(client);
@@ -52,13 +77,35 @@ int main(int argc, char* argv[])
 			continue;
 		}
 
-		//Display message and cient info
-		char clientIp[CLIENT_INFO_BUFFER_SIZE];
-		ZeroMemory(clientIp, CLIENT_INFO_BUFFER_SIZE);
+		char msgType = buf[0];
+		char_traits<char>::move(buf-1,buf, SERVER_BUFFER_SIZE);
 
-		inet_ntop(AF_INET, &client.sin_addr, clientIp, CLIENT_INFO_BUFFER_SIZE);
+		switch(msgType)
+		{
+			case '1'://join
+				char accept[SERVER_ACCEPT_MSG_BUFFER];
+				ZeroMemory(accept, SERVER_ACCEPT_MSG_BUFFER);
+				accept[0] = 1;
+				wsOk = sendto(in, accept, SERVER_ACCEPT_MSG_BUFFER, 0, reinterpret_cast<sockaddr*>(&client), sizeof(client));
 
-		cout << "Message recv from " << clientIp << " : " << buf << endl;
+				if(wsOk == SOCKET_ERROR)
+				{
+					cout << "Server failed sending acceptance message " << WSAGetLastError() << endl;
+				}
+				break;
+			case '2'://input
+				//Display message and cient info
+				char clientIp[CLIENT_INFO_BUFFER_SIZE];
+				ZeroMemory(clientIp, CLIENT_INFO_BUFFER_SIZE);
+
+				inet_ntop(AF_INET, &client.sin_addr, clientIp, CLIENT_INFO_BUFFER_SIZE);
+
+				cout << "Message recv from " << clientIp << " : " << buf << endl;
+				break;
+			default:
+				break;
+		}
+		
 	}
 
 	//Close socket
